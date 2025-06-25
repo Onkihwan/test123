f7594d44f75cf8a54f0ba17b0d08f09d02eb69fb
Onkihwan <chvndql2@naver.com>
2025-06-25 11:53:39 +0900

    opencv

---
+++ opencv.py
@@ -0,0 +1,18 @@
+import cv2
+
+# OpenCV 버전 출력
+print("OpenCV version:", cv2.__version__)
+
+# 간단한 이미지 생성 및 저장
+import numpy as np
+
+# 검은색 이미지 생성
+img = np.zeros((100, 100, 3), dtype=np.uint8)
+
+# 흰색 사각형 그리기
+cv2.rectangle(img, (20, 20), (80, 80), (255, 255, 255), -1)
+
+# 이미지 저장
+cv2.imwrite('test_image.png', img)
+
+print("Test image saved as 'test_image.png'")
