9a33c2005291701c3d1d76db65e7aa2ad66cb8da
Onkihwan <chvndql2@naver.com>
2025-06-27 16:24:33 +0900

    ^y^

---
+++ test1.py
@@ -0,0 +1,24 @@
+name = input()
+a = float(input())
+b = float(input())
+if name == '+':
+
+    print(a+b)
+
+elif name == '-':
+
+    print(a-b)
+
+elif name == '*':
+    print(a*b)
+elif name == '/':
+    if b==0:
+        print('zero division error')
+    else:
+        print(a/b)
+
+
+else : print('finish_^^;;iwantgohome')
+
+
+
